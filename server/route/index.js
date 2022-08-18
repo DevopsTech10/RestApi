dbf53645971e76562f9cdd95d7e98c8af3dec3ea
MuradCade <king12murad@gmail.com>
2022-08-17 22:07:35 -0700

    clean code

--- server/route/index.js
+++ server/route/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
@@ -14,6 +13,7 @@ router.get('/', async(req,res)=>{
     }
 
 })
+
 
 router.get('/:id',async(req,res)=>{
     
@@ -51,74 +51,4 @@ router.put('/:id',async(req,res)=>{
         res.sendStatus(500);
     }
 })
-
-
-const express = require('express');
-const router = express.Router();
-const db = require('../db');
-
-router.get('/', async(req,res)=>{
-
-    try {
-        let result = await db.all();
-        res.json(result);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-
-})
-
-router.get('/:id',async(req,res)=>{
-    
-    try {
-        let result = await db.one(req.params.id);
-        res.json(result);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-})
-
-router.post('/',async(req,res)=>{
-
-    
-    try {
-        let result = await db.add(req.body.username,req.body.gpa);
-        res.json(result);
-        
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-})
-
-router.put('/:id',async(req,res)=>{
-
-    
-    try {
-        let result = await db.update(req.body.id,req.body.username,req.body.gpa);
-        res.json(result);
-        
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-})
-const express = require('express');
-const router = express.Router();
-const db = require('../db');
-
-router.get('/', async(req,res)=>{
-
-    try {
-        let result = await db.all();
-        res.json(result);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-
-})
-
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const router  = require('./route');
@@ -11,17 +10,4 @@ app.use(express.json());
 app.use('/api/users',router);
 app.listen(process.env.PORT || '3000' ,()=>console.log(`Your Server Is Runnig On Port ${process.env.PORT || 3000}`))
 
-=======
-const express = require('express');
-const app = express();
-const router  = require('./route');
-const bodyparser = require('body-parser');
 
-app.use(bodyparser.urlencoded({extended: false}));
-app.use(express.json());
-
-
-app.use('/api/users',router);
-app.listen(process.env.PORT || '3000' ,()=>console.log(`Your Server Is Runnig On Port ${process.env.PORT || 3000}`))
-
->>>>>>> main
