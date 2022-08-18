2244e7636c03a9cc9c43c1cb4db5f764a69864db
MuradCade <king12murad@gmail.com>
2022-08-17 21:48:50 -0700

    updated some file

--- server/route/index.js
+++ server/route/index.js
@@ -14,4 +14,41 @@ router.get('/', async(req,res)=>{
 
 })
 
+router.get('/:id',async(req,res)=>{
+    
+    try {
+        let result = await db.one(req.params.id);
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+router.post('/',async(req,res)=>{
+
+    
+    try {
+        let result = await db.add(req.body.username,req.body.gpa);
+        res.json(result);
+        
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+router.put('/:id',async(req,res)=>{
+
+    
+    try {
+        let result = await db.update(req.body.id,req.body.username,req.body.gpa);
+        res.json(result);
+        
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
 module.exports = router;
