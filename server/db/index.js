1b186ff7901f3839aca2e56118fe9a8e2073a310
MuradCade <king12murad@gmail.com>
2022-08-17 22:11:34 -0700

    database structured

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require('mysql');
 const pool = mysql.createPool({
     host: 'localhost',
@@ -66,72 +65,3 @@ api.update =  (id,username,gpa) =>{
 }
 
 module.exports = api;
-=======
-const mysql = require('mysql');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    password: '',
-    database:'api'
-
-})
-
-if(pool){
-    console.log("db connected");
-}
-else{
-    console.log("db failed to connect");
-
-}
-let api = {};
-api.all = () =>{
-    return new Promise((resolve,reject) =>{
-        pool.query('select * from student',(err,result)=>{
-
-            if(err){
-                return reject(err);
-            }
-                return resolve(result);
-        });
-    });
-}
-
-
-api.one =  (id) =>{
-    return new Promise((resolve,reject)=>{
-        pool.query('select * from student where id = ?',[id], (err,result)=>{
-            if(err){
-                return reject(err);
-            }
-                return resolve(result[0]);
-        });
-    });
-}
-
-api.add =  (username,gpa) =>{
-    return new Promise((resolve,reject)=>{
-        pool.query("insert into student(username,gpa) value(??)",[username],[gpa], (err,result)=>{
-            if(err){
-                return reject(err);
-                console.log("error saving failed");
-            }
-                return resolve(result);
-                console.log("saved successfully");
-        });
-    });
-}
-api.update =  (id,username,gpa) =>{
-    return new Promise((resolve,reject)=>{
-        pool.query("Update from users SET username = ? , SET gpa = ? where id = ?",[username],[gpa], (err,result)=>{
-            if(err){
-                return reject(err);
-                console.log("error saving failed");
-            }
-                return resolve(result);
-                console.log("saved successfully");
-        });
-    });
-}
-
-module.exports = api;
->>>>>>> main
